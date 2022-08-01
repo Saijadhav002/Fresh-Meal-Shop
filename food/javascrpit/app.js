75f1ff68c6ad4fd44f92a27143c76abd94d94b49
Sai Jadhav <108349487+Saijadhav002@users.noreply.github.com>
2022-08-01 12:14:39 +0530

    Add files via upload

---
+++ food/javascrpit/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.food-slider').slick({
+ autoPlay:true,
+ slidesToShow:3,
+ slidesToScroll:1,
+ prevArrow:".prev-btn",
+ nextArrow:".next-btn",
+
+
+
+});
+});
---
+++ food/others/other1.js
@@ -0,0 +1,3 @@
+function myFunction (){
+    document.getElementById=("demo").innerHTML = "sai's first apporach to java script application";
+}
